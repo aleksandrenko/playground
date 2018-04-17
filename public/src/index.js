0d088b04ab23f06d25db05ca945cae42570447b2
Nikolai Aleksandrenko <aleksandrenko@gmail.com>
2018-04-17 10:45:35 +0300

    Serve static build version of the react app

--- public/src/index.js
+++ public/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 import { ApolloProvider } from 'react-apollo';
 import { client } from './ApolloClient';
@@ -13,5 +12,3 @@ ReactDOM.render(
     </ApolloProvider>,
     document.getElementById('root'),
 );
-
-registerServiceWorker();
