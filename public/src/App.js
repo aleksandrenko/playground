0a3557de08e6f41ba7f7f7d400280f1dfc46b374
Nikolai Aleksandrenko <aleksandrenko@gmail.com>
2018-04-17 17:48:33 +0300

    Rendering server types

--- public/src/App.js
+++ public/src/App.js
@@ -3,13 +3,70 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-function App({ data: { products, refetch } }) {
-    return (
-        <div>
-            <button onClick={() => refetch()}>Refresh</button>
-            <ul>{products && products.map(products => <li key={products.id}>{products.name}</li>)}</ul>
-        </div>
-    );
+import { HttpLink } from 'apollo-link-http';
+import fetch from 'node-fetch';
+
+import { introspectSchema, makeRemoteExecutableSchema } from 'graphql-tools';
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            schema: null,
+            isLoading: true
+        };
+    }
+
+    componentDidMount() {
+        const link = new HttpLink({ uri: 'http://localhost:3000/graphql/', fetch });
+
+        introspectSchema(link).then((schema) => {
+            const executableSchema = makeRemoteExecutableSchema({
+                schema,
+                link,
+            });
+
+            console.log(executableSchema); //Graphql server schema
+            console.log(schema); //Graphql server schema
+            console.log(executableSchema._queryType._fields);
+
+            this.setState({
+                schema: executableSchema,
+                isLoading: false
+            });
+        });
+    }
+
+    render() {
+        console.log(this.state);
+        const fields = this.state.schema
+            ? Object.values(this.state.schema._queryType._fields)
+            : [];
+
+        return (
+            <div>
+                { (this.props.loading || this.state.isLoading) &&
+                    <h1>loading</h1>
+                }
+
+                <h1>hello</h1>
+                types: {
+                    fields.map(field => {
+                        const url = `./fields/${field.name}/`;
+                        const name = field.name;
+                        return <div>
+                            <a href={url} key={name}>{ name }</a>
+                        </div>;
+                    })
+                }
+
+                {/*<button onClick={() => refetch()}>Refresh</button>*/}
+                {/*<ul>{products && products.map(products => <li key={products.id}>{products.name}</li>)}</ul>*/}
+            </div>
+        );
+    }
 }
 
 export default graphql(gql`
--- public/src/index.js
+++ public/src/index.js
@@ -6,24 +6,6 @@ import App from './App';
 import { ApolloProvider } from 'react-apollo';
 import { client } from './ApolloClient';
 
-import { HttpLink } from 'apollo-link-http';
-import fetch from 'node-fetch';
-
-import { introspectSchema, makeRemoteExecutableSchema } from 'graphql-tools';
-const link = new HttpLink({ uri: 'http://localhost:3000/graphql/', fetch });
-
-introspectSchema(link).then((schema) => {
-    const executableSchema = makeRemoteExecutableSchema({
-        schema,
-        link,
-    });
-
-    console.log(executableSchema); //Graphql server schema
-    console.log(executableSchema._queryType._fields);
-});
-
-
-
 ReactDOM.render(
     <ApolloProvider client={client}>
         <App />
