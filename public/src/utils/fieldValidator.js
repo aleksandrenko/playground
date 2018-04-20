4cc8e9327c40591e9d7f8e7aabaa27c0419c322d
Nikolai Aleksandrenko <aleksandrenko@gmail.com>
2018-04-20 16:27:06 +0300

    Basic doc for the ui field validation to be implemented

---
+++ public/src/utils/fieldValidator.js
@@ -0,0 +1,47 @@
+/*
+    Types:
+    Date:
+        @Date
+        @Future
+        @FutureOrPresent
+        @Past
+        @PastOrPresent
+
+    Email
+        @Email
+
+    Number
+        @Number
+        @Precision(10)
+        @Positive
+        @PositiveOrZero
+
+    Int
+        @Positive
+        @PositiveOrZero
+
+    Url
+        @Url
+
+    File
+        @File
+
+    @Min(1)
+    @Max(10)
+
+
+
+    @Negative
+    @negativeOrZero
+
+
+    @Default(516)
+
+    @NoUserInput
+    @NoUI
+*/
+
+
+export default ({field, value}) => {
+    return '';
+}
--- schema/products.js
+++ schema/products.js
@@ -1,9 +1,70 @@
+/*
+    GraphQL basic types:
+
+    Int: A signed 32‐bit integer.
+    Float: A signed double-precision floating-point value.
+    String: A UTF‐8 character sequence.
+    Boolean: true or false.
+    ID: The ID scalar type represents a unique identifier, often used to refetch an object or as the key for a cache.
+        The ID type is serialized in the same way as a String; however, defining it as an ID signifies that it is not
+        intended to be human‐readable.
+
+    Custom types:
+
+    @Date
+        @Format(mm:dd:yyyy)
+        @Future: The date is valid only if in the future
+        @FutureOrPresent: The date is valid only if in the future or present
+        @Past: The date is valid only if in the past
+        @PastOrPresent: The date is valid only if in the past or present
+
+    @Email: A text field with email validation
+
+    @File: Will show a browse UI element
+    @URL: A field validation url string
+
+    Additional validation:
+        @Precision(10): Cam ne use for float types, specifies the digits after the decimal sign
+
+        @Positive: Can be used for int and float
+        @PositiveOrZero: Can be used for int and float
+        @Negative: Can be used for int and float
+        @negativeOrZero: Can be used for int and float
+
+        @Min(1): Can be used for email, url, string, number, float
+        @Max(10): Can be used for email, url, string, number, float
+
+    Custom Behavior:
+        @Default(516): The ui element for this field will use this as initial value
+
+        @NoUserInput: Will not show the field as readonly in the UI
+        @NoUI: Will not render any ui for the field
+*/
+
 export const ProductType = `
     type Product {
-        id: Int,
-        name: String,
-        description: String,
-        img: String,
+        # @NoUserInput 
+        id: Int
+        
+        # @Min(3) @Max(20)
+        name: String
+        
+        # @Min(3) @Max(20) @Email
+        email: String
+        
+        # @Default(true)
+        isPublic: Boolean
+        
+        # Description of the product
+        description: String
+        
+        # Photo of the product @File
+        img: String
+        
+        # Date of Creation @Date
+        creatingDate: String
+        
+        # All product ingredients
         ingredients: [Ingredient]
     }
 `;
